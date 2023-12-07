85f01fb4493f87d09a58a2a42f283b256372a821
Daniel Terhorst-North <daniel@dannorth.net>
2023-12-07 21:13:39 +0000

    store results in `main`

--- main.go
+++ main.go
@@ -14,12 +14,12 @@ func main() {
 
 func solveDay1() {
 	lines := strings.Split(input.ForDay(1), "\n")
-	println("Answer for day 1 part 1: ", day1.SumEnds(lines))
-	println("Answer for day 2 part 1: ", day1.SumEndsWithWords(lines))
+	println("Answer for day 1 part 1: ", day1.SumEnds(lines))          // 56465
+	println("Answer for day 2 part 1: ", day1.SumEndsWithWords(lines)) // 55902
 }
 
 func solveDay2() {
 	lines := strings.Split(input.ForDay(2), "\n")
-	println("Answer for day 2 part 1: ", day2.SumPossibleIds(lines, day2.GameLimits))
-	println("Answer for day 2 part 2: ", day2.SumPowersOfGames(lines))
+	println("Answer for day 2 part 1: ", day2.SumPossibleIds(lines, day2.GameLimits)) // 2600
+	println("Answer for day 2 part 2: ", day2.SumPowersOfGames(lines))                // 86036
 }
